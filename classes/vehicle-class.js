34cd1f75fbd6cbac9033d3525cd14e6300564c73
Ian Smith <ianmsmith17@gmail.com>
2019-09-17 14:29:06 -0500

    part 1

--- classes/vehicle-class.js
+++ classes/vehicle-class.js
@@ -1,6 +1,39 @@
 'use strict';
 
 class Car {
+  constructor() {
+    this.wheels = 4;
+
+  }
+
+  drive() {
+    return true;
+  }
+
+  stop() {
+    return true;
+  }
 }
 
-module.exports = Car;
+class Motorcycle {
+  constructor() {
+    this.wheels = 2;
+  }
+
+  drive() {
+    return true;
+  }
+
+  stop() {
+    return true;
+  }
+
+  wheelie() {
+    return true;
+  }
+}
+
+module.exports = {
+  Car,
+  Motorcycle,
+};
