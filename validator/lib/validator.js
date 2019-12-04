d29e0ad640d31373684310d1f5087be37a5bd7ff
Ian Smith <ianmsmith17@gmail.com>
2019-12-04 12:51:55 -0600

    finished lab-02

--- validator/lib/validator.js
+++ validator/lib/validator.js
@@ -9,81 +9,78 @@ class Validator {
  * @param rules
  * @returns {boolean}
  */
-isValid(input, rules) {
-  return true;
-};
 
-/**
+  /**
  * Is this a string?
  * @param input
  * @returns {boolean}
  */
-isString(input) {
-  return typeof input === 'string';
-};
-
-isObject(input) {
-  return typeof input === 'object' && !(input instanceof Array);
-};
-
-isArray(input, valueType) {
-  return Array.isArray(input) && (valueType ? input.every( val => typeof val === valueType ) : true);
-};
-
-isBoolean(input) {
-  return typeof input === 'boolean';
-};
-
-isNumber(input) {
-  return typeof input === 'number';
-};
-
-isFunction(input) {
-  return typeof input === 'function';
-};
-
-isTruthy(input) {
-  return !!input;
-};
-
-isCorrectType(input, field) {
-  switch(field.type) {
-  case 'string': return this.isString(input);
-  case 'number': return this.isNumber(input);
-  case 'array': return this.isArray(input, field.valueType);
-  case 'object': return this.isObject(input);
-  case 'boolean': return this.isBoolean(input);
-  default: return false;
+  isString(input) {
+    return typeof input === 'string';
   }
-};
 
-isValid(schema,data) {
+  isObject(input) {
+    return typeof input === 'object' && !(input instanceof Array);
+  }
 
-  let valid = true;
+  isArray(input, valueType) {
+    return Array.isArray(input) && (valueType ? input.every( val => typeof val === valueType ) : true);
+  }
 
-  for (let fieldName in schema.fields) {
+  isBoolean(input) {
+    return typeof input === 'boolean';
+  }
 
-    let field = schema.fields[fieldName];
+  isNumber(input) {
+    return typeof input === 'number';
+  }
 
-    // Am I required and set?
-    let required = field.required
-      ? this.isTruthy(data[fieldName])
-      : true;
+  isFunction(input) {
+    return typeof input === 'function';
+  }
+
+  isTruthy(input) {
+    return !!input;
+  }
+
+  isCorrectType(input, field) {
+    switch(field.type) {
+    case 'string': return this.isString(input);
+    case 'number': return this.isNumber(input);
+    case 'array': return this.isArray(input, field.valueType);
+    case 'object': return this.isObject(input);
+    case 'boolean': return this.isBoolean(input);
+    default: return false;
+    }
+  }
+
+  isValid(schema,data) {
+
+    let valid = true;
+
+    for (let fieldName in schema.fields) {
+
+      let field = schema.fields[fieldName];
+
+      // Am I required and set?
+      let required = field.required
+        ? this.isTruthy(data[fieldName])
+        : true;
 
       // Am I the right type (if we even care)
-    let type = field.type
-      ? this.isCorrectType(data[fieldName], field)
-      : true;
+      let type = field.type
+        ? this.isCorrectType(data[fieldName], field)
+        : true;
 
       // If anything is false ...
-    if (!(required && type)) {
-      valid = false;
+      if (!(required && type)) {
+        valid = false;
+      }
+
     }
-
+    
+    return valid;
   }
-
-  return valid;
-};
 }
 
 
